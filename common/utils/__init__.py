0eb28860b6990c08f56648f1a8a22c1584086781
Arbbi Cobult <alex.stevens@cybermadesimple.co.uk>
2025-10-02 09:20:48 +0100

    Enhance audit logging sensitivity tracking

--- common/utils/__init__.py
+++ common/utils/__init__.py
@@ -1,6 +1,13 @@
 """Utilities for application-wide helpers."""
 
 from . import audit_logger as audit_logger
-from .audit_logger import hash_ip, log_audit, main, verify_audit_chain
+from .audit_logger import hash_ip, is_sensitive_action, log_audit, main, verify_audit_chain
 
-__all__ = ["audit_logger", "hash_ip", "log_audit", "main", "verify_audit_chain"]
+__all__ = [
+    "audit_logger",
+    "hash_ip",
+    "is_sensitive_action",
+    "log_audit",
+    "main",
+    "verify_audit_chain",
+]
