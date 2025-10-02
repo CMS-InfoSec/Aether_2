97b8dae34a58cb8aa0bcfed5e6bd5b9cc54347a8
Arbbi Cobult <alex.stevens@cybermadesimple.co.uk>
2025-10-02 23:43:17 +0100

    Add simulated broker with configurable fills

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,12 @@
 """Configuration package for service stubs."""
 
 from .risk_limits import AccountRiskLimits, ConfigError, get_account_limits
+from .simulation import SimulationConfig, get_simulation_config
 
 __all__ = [
     "AccountRiskLimits",
     "ConfigError",
     "get_account_limits",
+    "SimulationConfig",
+    "get_simulation_config",
 ]
