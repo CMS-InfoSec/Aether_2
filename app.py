f805cdac565c3d617545c2a22d693d390b4e7128
Arbbi Cobult <alex.stevens@cybermadesimple.co.uk>
2025-10-02 11:19:34 +0100

    Import scaling controller symbols explicitly in app

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ from alert_prioritizer import router as alert_prioritizer_router
 from services.report_service import router as reports_router
 from multiformat_export import router as log_export_router
 from compliance_pack import router as compliance_router
+from scaling_controller import (
+    build_scaling_controller_from_env,
+    configure_scaling_controller,
+    router as scaling_router,
+)
 
 from pack_exporter import router as knowledge_router
 
