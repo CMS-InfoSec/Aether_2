3990da57619c61c11250d72d41ab79b4a3bfb66c
Arbbi Cobult <alex.stevens@cybermadesimple.co.uk>
2025-10-02 09:24:05 +0100

    Add intent backpressure controller

--- services/core/__init__.py
+++ services/core/__init__.py
@@ -1,5 +1,18 @@
 """Core service primitives shared across the control plane."""
 
+from .backpressure import (
+    BackpressureStatus,
+    IntentBackpressure,
+    app as backpressure_app,
+    backpressure_controller,
+)
 from .sequencer import SequencerResult, TradingSequencer
 
-__all__ = ["SequencerResult", "TradingSequencer"]
+__all__ = [
+    "BackpressureStatus",
+    "IntentBackpressure",
+    "SequencerResult",
+    "TradingSequencer",
+    "backpressure_app",
+    "backpressure_controller",
+]
