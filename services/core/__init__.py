d8dd64e9a997da89aa3eb4228a03f139ee3325b7
Arbbi Cobult <alex.stevens@cybermadesimple.co.uk>
2025-10-02 09:24:47 +0100

    Add startup manager to bootstrap control plane state

--- services/core/__init__.py
+++ services/core/__init__.py
@@ -1,5 +1,18 @@
 """Core service primitives shared across the control plane."""
 
 from .sequencer import SequencerResult, TradingSequencer
+from .startup_manager import (
+    StartupManager,
+    StartupMode,
+    register as register_startup_manager,
+    router as startup_router,
+)
 
-__all__ = ["SequencerResult", "TradingSequencer"]
+__all__ = [
+    "SequencerResult",
+    "TradingSequencer",
+    "StartupManager",
+    "StartupMode",
+    "register_startup_manager",
+    "startup_router",
+]
